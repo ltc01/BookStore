58c6c7d85442110dff2a88be208faaab651c635a
ltc01 <laxmichoudhary909@gmail.com>
2024-03-15 21:01:53 +0530

    Fixed configuarations

--- Backend/config.js
+++ Backend/config.js
@@ -1,2 +1,5 @@
-export const Port = 5000;
-export const mongoUrl = 'mongodb+srv://laxmiDb:laxmi123@cluster0.lepoh8f.mongodb.net/?retryWrites=true&w=majority';
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const Port = process.env.PORT;
+export const mongoUrl = process.env.MONGO_URL;
